3aaabed4cab32a9b745caef883ee714ec6d7a5cb
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-07-01 13:11:17 -0500

    add corsConfigurer returning WebMvcConfigurer

--- src/main/java/com/hervey/app/ProductsCustomersApplication.java
+++ src/main/java/com/hervey/app/ProductsCustomersApplication.java
@@ -5,11 +5,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProductsCustomersApplication {
-
-	public static void main(String[] args) {
+		
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				//registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+	
+//		@Bean
+//	    public void addCorsMappings(CorsRegistry registry) {
+//	        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+//            .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+//                    "Access-Control-Request-Headers")
+//            .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+//            .allowCredentials(true).maxAge(3600);
+//	    }
+		public static void main(String[] args) {
 		SpringApplication.run(ProductsCustomersApplication.class, args);
 	}
     @Bean
