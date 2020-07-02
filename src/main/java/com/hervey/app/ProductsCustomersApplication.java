8365d6bd68745bd32c96ca58272c32dc1434348f
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-07-02 17:43:49 -0500

    Modified Cors with two new attributes

--- src/main/java/com/hervey/app/ProductsCustomersApplication.java
+++ src/main/java/com/hervey/app/ProductsCustomersApplication.java
@@ -18,19 +18,54 @@ public class ProductsCustomersApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				//registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("PUT", "DELETE","GET", "POST") //added 2nd
+				.allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers") //added 2nd
+				.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials") //hesitated, but added 2nd
+				;
 			}
 		};
 	}
+/*
+		@Bean
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+            .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+            .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+            .allowCredentials(true).maxAge(3600);
+	    }
+	    
+*/
 	
-//		@Bean
-//	    public void addCorsMappings(CorsRegistry registry) {
-//	        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
-//            .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-//                    "Access-Control-Request-Headers")
-//            .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-//            .allowCredentials(true).maxAge(3600);
-//	    }
+/*
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	            System.out.println("allow origin: "+allowOrigins);
+	            return new WebMvcConfigurerAdapter() {
+	                @Override
+	                public void addCorsMappings(CorsRegistry registry) {
+	                    registry.addMapping("/**")
+	                    //.allowedOrigins("http://localhost")
+	                    .allowedOrigins(allowOrigins)
+	                    .allowedMethods("PUT", "DELETE","GET", "POST");
+	                }
+	            };
+	        }
+	
+*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 		public static void main(String[] args) {
 		SpringApplication.run(ProductsCustomersApplication.class, args);
 	}
